0fa470c562912a2c0d8adfbf0cedefcda22746e4
Mirko Malesic <mikromal@gmail.com>
2015-09-28 05:46:27 -0400

    Zadnja z bazo,ki dela

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -10,7 +10,7 @@ class SignupsController < ApplicationController
     	redirect_to thanks_path
     end
   end
-  
+ 
   private 
     def signup_params
       params.require(:signup).permit(:firstname, :email)

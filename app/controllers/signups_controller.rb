47183b900e25fc74456cc6fb799ac9fe0064ce24
Mirko Malesic <mikromal@gmail.com>
2015-09-28 05:02:38 -0400

    Final commit

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,19 @@
+class SignupsController < ApplicationController
+  def new
+  	@signup = Signup.new
+  end
+  
+	def create
+    @signup = Signup.new(signup_params)
+    if @signup.save
+      session[:signup_id] = @signup.id
+    	redirect_to thanks_path
+    end
+  end
+  
+  private 
+    def signup_params
+      params.require(:signup).permit(:firstname, :email)
+    end
+ 
+end
